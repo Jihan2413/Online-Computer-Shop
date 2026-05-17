165bc4e8eb065c1e0de32ad2b53d694fbd1cb2d0
Osama-Mobin <zuharbhai535@gmail.com>
2026-05-17 20:04:58 +0600

    Add footer, header, and product card layout files

---
+++ views/layout/footer.php
@@ -0,0 +1,12 @@
+<?php
+
+?>
+</div>
+
+<footer class="site-footer">
+    <p>© <?php echo date('Y'); ?> PC Shop &nbsp;|&nbsp; Online Computer Shop – Task 3</p>
+</footer>
+
+<script src="<?php echo $rootPath ?? ''; ?>public/assets/js/shop.js"></script>
+</body>
+</html>
